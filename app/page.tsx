995f5a205484969de10af412aa4c24ece00486c4
Slackz0r <gustav.aldenberg@gmail.com>
2026-02-20 10:33:51 +0100

    refactor fetch function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import type { ProductsResponse } from "./types";
+import { getData } from "@/utils/fetchFunctions";
+import { ProductsResponse } from "./types";
 
-const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
 export default async function Home() {
@@ -8,17 +8,20 @@ export default async function Home() {
   // in this fetch we sort using _sort and _order and we limit the number of products using _limit
   // we also use _expand to get the relational category data
   // we can use the other destructed variables like page, total and so on to create pagination or show info
-  const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
-  ).then((res) => res.json());
+  const data: ProductsResponse = await getData(defaultLimit);
 
-
-console.log(products);
+  console.log(data);
 
   return (
     <main>
       <h1>Products</h1>
-      <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
+      <div>
+        {data.products.map((product) => (
+          <h2 key={product.id}>
+            {product.title} - {product.category?.name}
+          </h2>
+        ))}
+      </div>
     </main>
   );
 }

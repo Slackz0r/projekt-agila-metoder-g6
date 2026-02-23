1f272954e406d3596a68c55ba82b5b2113b38c18
Fredrik.N <fredrik.nelin@outlook.com>
2026-02-23 10:03:59 +0100

    Changed button to Link

--- components/ui/product-management-button.tsx
+++ components/ui/product-management-button.tsx
@@ -1,17 +1,12 @@
-"use client";
-
-function addButton() {
-    alert("Lägg till kod för att visa ett formulär med fält för att fylla i produktinformation")
-
-}
+import Link from "next/link";
 
 export default function Button() {
     return (
         <>
-            <button type="submit" onClick={addButton} className="flex items-center justify-between px-4 w-36 h-10 bg-purple-900 text-gray-50 rounded-md cursor-pointer">
+            <Link href="/" className="flex items-center justify-between px-4 w-36 h-10 bg-purple-900 text-gray-50 rounded-md cursor-pointer">
                 <span className="text-2xl leading-none relative -top-0.5">+</span>
                 <span className="leading-none">Add product</span>
-            </button >
+            </Link>
         </>
     )
 }

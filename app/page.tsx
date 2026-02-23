3bad4924109c752ab911703629d3c228f8df6ef3
fneel <70952301+fneel@users.noreply.github.com>
2026-02-23 14:33:43 +0100

    app-page-div - flex

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ProductsResponse } from "@/types/types.ts";
 //Components
 import AdminPanel from "@/components/ui/admin-panel";
 //import ProductGrid from "@/components/ui/product-grid";
-import Products, {ProdLen} from "@/components/GetProd"
+import Products, { ProdLen } from "@/components/GetProd";
 import ProductManagement from "@/components/ui/product-management";
 import ProductStats from "@/components/ui/product-stats";
 import SearchBar from "@/components/ui/search-bar";
@@ -18,18 +18,18 @@ const API_URL = "http://localhost:4000";
 export default async function Home() {
   const data: ProductsResponse = await getData(defaultLimit);
 
-console.log(data);
+  console.log(data);
 
   return (
     <main>
-    <p>{ProdLen(data.products)}</p>
-      <div>
+      <p>{ProdLen(data.products)}</p>
+      <div className="flex">
         <AdminPanel />
         <section>
           <ProductManagement />
           <ProductStats />
           <SearchBar />
-	  <Products />
+          <Products />
         </section>
       </div>
     </main>

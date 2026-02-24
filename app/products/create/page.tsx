dac168a92a65edc95c1d144a657269aa75a3321a
Fredrik.N <fredrik.nelin@outlook.com>
2026-02-24 08:14:03 +0100

    Ändrade ordningen på formulärfälten.

--- app/products/create/page.tsx
+++ app/products/create/page.tsx
@@ -16,122 +16,123 @@ const categories: Category[] = await fetch(`http://localhost:4000/categories`).t
 
 
 export default function createProductPage() {
-    return <main className="container mx-auto bg-gray-100 p-4">
-        <h1
-            className="
-                text-3xl md:text-4xl
-                font-semibold
-                tracking-tight
-                text-gray-900
-                m-6"
-        >
-            Lägg till en produkt
-        </h1>
-        <Form action={addProduct}>
-            <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 items-center max-w-4xl">
-                <label className="font-semibold" htmlFor="title">
-                    Titel
-                </label>
-                <input
-                    className={styleInput}
-                    type="text"
-                    id="title"
-                    name="title"
-                    minLength={3}
-                    maxLength={20}
-                    required
-                />
-                <label className="font-semibold" htmlFor="brand">
-                    Märke
-                </label>
-                <input
-                    className={styleInput}
-                    type="text"
-                    id="brandprice"
-                    name="brand"
-                    required
-                />
-                <label className="font-semibold" htmlFor="price">
-                    Pris
-                </label>
-                <input
-                    className={styleInput}
-                    type="number"
-                    id="price"
-                    name="price"
-                    min="1.0"
-                    step="0.01"
-                    required
-                />
-                <label className="font-semibold" htmlFor="stock">
-                    Lagerantal
-                </label>
-                <input
-                    className={styleInput}
-                    type="number"
-                    id="stock"
-                    name="stock"
-                    min="0"
-                    step="1"
-                />
-                <label className="font-semibold" htmlFor="categoryId">
-                    Kategori ID
-                </label>
-                <select
-                    className={styleInput}
-                    id="categoryId"
-                    name="categoryId"
-                    required
-                >
-                    {categories.map((cat) => <option key={`categoryOption-${cat.id}`} value={cat.id}>{cat.name}</option>)}
-                </select>
+    return <main className="bg-slate-100 min-h-screen flex items-center justify-center">
+        <div className="w-full max-w-2xl px-6">
 
-                <label className="font-semibold" htmlFor="description">
-                    Beskrivning
-                </label>
-                <textarea
-                    className={styleTextArea}
-                    id="description"
-                    name="description"
-                    minLength={5}
-                    maxLength={400}
-                    required
-                />
-                <label className="font-semibold" htmlFor="thumbnail">
-                    Miniatyrbild
-                </label>
-                <input
-                    className={styleInput}
-                    type="url"
-                    id="thumbnail"
-                    name="thumbnail"
-                    required
-                />
-                <span className="col-span-2 flex justify-center">
-                    <button
-                        type="submit"
-                        id="submit"
-                        name="submit"
-                        value="again"
-                        className={styleButton}
-                    >Spara och igen
-                    </button>
-                    <button
-                        type="submit"
-                        id="submit"
-                        name="submit"
-                        value="back"
-                        className={styleButton}
-                    >Spara och tillbaka
-                    </button>
-                    <button
-                        type="reset"
-                        className={styleButton}
-                    >Ångra
-                    </button>
-                </span>
-            </div >
-        </Form >
+            <div className="mx-auto w-max">
+                <h1 className="text-2xl font-semibold text-slate-800">
+                    Lägg till en produkt
+                </h1>
+                <p className="text-sm text-slate-500">Skriv in följande produktinformation</p>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-sm p-8">
+                <Form action={addProduct}>
+                    <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 items-center max-w-4xl">
+                        <label className="font-semibold" htmlFor="title">
+                            Titel
+                        </label>
+                        <input
+                            className={styleInput}
+                            type="text"
+                            id="title"
+                            name="title"
+                            minLength={3}
+                            maxLength={20}
+                            required
+                        />
+                        <label className="font-semibold" htmlFor="brand">
+                            Märke
+                        </label>
+                        <input
+                            className={styleInput}
+                            type="text"
+                            id="brandprice"
+                            name="brand"
+                            required
+                        />
+                        <label className="font-semibold" htmlFor="categoryId">
+                            Kategori ID
+                        </label>
+                        <select
+                            className={styleInput}
+                            id="categoryId"
+                            name="categoryId"
+                            required
+                        >
+                            {categories.map((cat) => <option key={`categoryOption-${cat.id}`} value={cat.id}>{cat.name}</option>)}
+                        </select>
+                        <label className="font-semibold" htmlFor="price">
+                            Pris
+                        </label>
+                        <input
+                            className={styleInput}
+                            type="number"
+                            id="price"
+                            name="price"
+                            min="1.0"
+                            step="0.01"
+                            required
+                        />
+                        <label className="font-semibold" htmlFor="stock">
+                            Lagerantal
+                        </label>
+                        <input
+                            className={styleInput}
+                            type="number"
+                            id="stock"
+                            name="stock"
+                            min="0"
+                            step="1"
+                        />
+                        <label className="font-semibold" htmlFor="thumbnail">
+                            Miniatyrbild
+                        </label>
+                        <input
+                            className={styleInput}
+                            type="url"
+                            id="thumbnail"
+                            name="thumbnail"
+                            required
+                        />
+                        <label className="font-semibold" htmlFor="description">
+                            Beskrivning
+                        </label>
+                        <textarea
+                            className={styleTextArea}
+                            id="description"
+                            name="description"
+                            minLength={5}
+                            maxLength={400}
+                            required
+                        />
+                        <span className="col-span-2 flex justify-center">
+                            <button
+                                type="submit"
+                                id="submit"
+                                name="submit"
+                                value="again"
+                                className={styleButton}
+                            >Spara och igen
+                            </button>
+                            <button
+                                type="submit"
+                                id="submit"
+                                name="submit"
+                                value="back"
+                                className={styleButton}
+                            >Spara och tillbaka
+                            </button>
+                            <button
+                                type="reset"
+                                className={styleButton}
+                            >Ångra
+                            </button>
+                        </span>
+                    </div >
+                </Form >
+            </div>
+        </div>
     </main>
 
 }

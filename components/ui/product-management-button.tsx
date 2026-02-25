46e959405ed1c1a6812596b65b93acbf25b8d967
Fredrik.N <fredrik.nelin@outlook.com>
2026-02-25 13:42:20 +0100

    changed button color to same as hover color on admin panel

--- components/ui/product-management-button.tsx
+++ components/ui/product-management-button.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Button() {
-    const styleButton = "inline-flex items-center justify-center rounded-xl bg-purple-700 px-5 py-2.5 mx-8 text-sm font-medium text-white shadow-sm transition-all duration-200 ease-in-out hover:bg-purple-800 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500/40 focus:ring-offset-2 active:scale-[0.98] disabled:bg-purple-300 disabled:cursor-not-allowed disabled:shadow-none";
+    const styleButton = "inline-flex items-center justify-center rounded-xl bg-fuchsia-950 px-5 py-2.5 mx-8 text-sm font-medium text-white shadow-sm transition-all duration-200 ease-in-out hover:bg-fuchsia-950 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500/40 focus:ring-offset-2 active:scale-[0.98] disabled:bg-fuchsia-300 disabled:cursor-not-allowed disabled:shadow-none";
     return (
         <>
             <Link href="/products/create" className={styleButton}>

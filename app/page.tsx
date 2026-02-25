06e33cc3088371aa991e86f19571c34c8a3fb537
fneel <70952301+fneel@users.noreply.github.com>
2026-02-25 11:03:17 +0100

    removed line 7 from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { ProductsResponse } from "@/types/types";
 //Components
 import AdminPanel from "@/components/ui/admin-panel";
 //import ProductGrid from "@/components/ui/product-grid";
-import Products, { ProdLen } from "@/components/GetProd";
 import ProductManagement from "@/components/ui/product-management";
 import ProductStats from "@/components/ui/product-stats";
 import SearchBar from "@/components/ui/search-bar";
